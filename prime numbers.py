ad6fccd586220a1085206ebb2f8695b0dc99465f
mahtab-talebzadeh <mahtab.talebzadeh2018@gmail.com>
2025-05-28 02:10:39 -0700

    Update prime numbers.py

--- prime numbers.py
+++ prime numbers.py
@@ -8,10 +8,10 @@ if n>=2:
     while i<=length:
         if n%i==0:
             flag=False
+            print("It is not a prime number") 
+            break
         i+=1
     if flag:
         print("It's a prime number")
-    else:
-        print("It is not a prime number")  
 else:
     print("The first prime number is two")
